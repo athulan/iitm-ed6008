0fd1af22db5a9a69ed0d38bc0842db7d057623ff
Athulan Vijayaraghavan <athulan@systeminsights.com>
2021-12-28 09:41:25 +0530

    tiny change

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Course Overview
+            Introduction
           </Link>
         </div>
       </div>
